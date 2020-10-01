74310c6b28e15faabab15c2cb5ed6f73e67c2908
rushil-skywalker <62181313+rushil-skywalker@users.noreply.github.com>
2020-10-01 22:47:02 +0530

    Fixed Bugs

--- Main.java
+++ Main.java
@@ -1,9 +1,11 @@
 import java.util.*;
 public class Main {
     public static void main(String[] args) {
+        // Making Scanner class
         Scanner sc = new Scanner(System.in);
+        // Making infinite loop
         while (true) {
-            System.out.println("Chhose an operation");
+            System.out.println("Choose an operation");
             System.out.println("Press 1 for Addition");
             System.out.println("Press 2 for Subtraction");
             System.out.println("Press 3 for Multiplication");
@@ -11,8 +13,9 @@ public class Main {
             int operation = sc.nextInt();
             System.out.println("Enter first no.");
             int no1 = sc.nextInt();
-            System.out.println("Enter secon no.");
+            System.out.println("Enter second no.");
             int no2 = sc.nextInt();
+            // Main Code for working of code 
             if (operation == 1) {
                 System.out.println(no1 + no2);
             }
@@ -22,6 +25,9 @@ public class Main {
             else if (operation == 3) {
                 System.out.println(no1 * no2);
             }
+            else if (operation == 4 && no2 == 0) {
+                System.out.println("Invalid Second No.");
+            }   
             else if (operation == 4) {
                 System.out.println(no1 / no2);
             }
